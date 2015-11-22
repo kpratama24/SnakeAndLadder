e92f3d474e4f1759787887fdcd530a322cbf58e3
Kevin Pratama <admin@blankevin.com>
2015-11-22 22:55:32 +0700

    Create Form2.Designer.cs
    initial

---
+++ src/engine/Form2.Designer.cs
@@ -0,0 +1,352 @@
+namespace Snake_and_Ladder
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.Player1Name = new System.Windows.Forms.Label();
+            this.Player2Name = new System.Windows.Forms.Label();
+            this.CurrentPlayer = new System.Windows.Forms.Label();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Exit = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Dice2 = new System.Windows.Forms.PictureBox();
+            this.Dice3 = new System.Windows.Forms.PictureBox();
+            this.Dice4 = new System.Windows.Forms.PictureBox();
+            this.Dice5 = new System.Windows.Forms.PictureBox();
+            this.Dice6 = new System.Windows.Forms.PictureBox();
+            this.Dice1 = new System.Windows.Forms.PictureBox();
+            this.Board1 = new System.Windows.Forms.PictureBox();
+            this.Board2 = new System.Windows.Forms.PictureBox();
+            this.Board3 = new System.Windows.Forms.PictureBox();
+            this.Board4 = new System.Windows.Forms.PictureBox();
+            this.Board5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player1Name
+            // 
+            this.Player1Name.BackColor = System.Drawing.Color.Aquamarine;
+            this.Player1Name.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Name.Location = new System.Drawing.Point(940, 317);
+            this.Player1Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Player1Name.Name = "Player1Name";
+            this.Player1Name.Size = new System.Drawing.Size(212, 30);
+            this.Player1Name.TabIndex = 54;
+            this.Player1Name.Text = "Name Here";
+            this.Player1Name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Player2Name
+            // 
+            this.Player2Name.BackColor = System.Drawing.Color.Aquamarine;
+            this.Player2Name.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Name.Location = new System.Drawing.Point(940, 347);
+            this.Player2Name.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Player2Name.Name = "Player2Name";
+            this.Player2Name.Size = new System.Drawing.Size(212, 32);
+            this.Player2Name.TabIndex = 55;
+            this.Player2Name.Text = "Name Here";
+            this.Player2Name.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CurrentPlayer
+            // 
+            this.CurrentPlayer.BackColor = System.Drawing.Color.SaddleBrown;
+            this.CurrentPlayer.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CurrentPlayer.Location = new System.Drawing.Point(988, 417);
+            this.CurrentPlayer.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.CurrentPlayer.Name = "CurrentPlayer";
+            this.CurrentPlayer.Size = new System.Drawing.Size(158, 52);
+            this.CurrentPlayer.TabIndex = 67;
+            this.CurrentPlayer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.Red;
+            this.Player2.Image = ((System.Drawing.Image)(resources.GetObject("Player2.Image")));
+            this.Player2.Location = new System.Drawing.Point(53, 646);
+            this.Player2.Margin = new System.Windows.Forms.Padding(4);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(33, 31);
+            this.Player2.TabIndex = 66;
+            this.Player2.TabStop = false;
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.Color.Blue;
+            this.Player1.Image = ((System.Drawing.Image)(resources.GetObject("Player1.Image")));
+            this.Player1.Location = new System.Drawing.Point(53, 684);
+            this.Player1.Margin = new System.Windows.Forms.Padding(4);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(33, 31);
+            this.Player1.TabIndex = 65;
+            this.Player1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(916, 475);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(257, 64);
+            this.button2.TabIndex = 64;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.Cyan;
+            this.Exit.Image = ((System.Drawing.Image)(resources.GetObject("Exit.Image")));
+            this.Exit.Location = new System.Drawing.Point(943, 663);
+            this.Exit.Margin = new System.Windows.Forms.Padding(4);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(109, 52);
+            this.Exit.TabIndex = 63;
+            this.Exit.UseVisualStyleBackColor = false;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.Aqua;
+            this.Reset.Image = ((System.Drawing.Image)(resources.GetObject("Reset.Image")));
+            this.Reset.Location = new System.Drawing.Point(1060, 663);
+            this.Reset.Margin = new System.Windows.Forms.Padding(4);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(103, 52);
+            this.Reset.TabIndex = 62;
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Dice2
+            // 
+            this.Dice2.Image = global::Snake_and_Ladder.Properties.Resources._2;
+            this.Dice2.Location = new System.Drawing.Point(983, 539);
+            this.Dice2.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice2.Name = "Dice2";
+            this.Dice2.Size = new System.Drawing.Size(121, 95);
+            this.Dice2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice2.TabIndex = 61;
+            this.Dice2.TabStop = false;
+            // 
+            // Dice3
+            // 
+            this.Dice3.Image = global::Snake_and_Ladder.Properties.Resources._3;
+            this.Dice3.Location = new System.Drawing.Point(983, 539);
+            this.Dice3.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice3.Name = "Dice3";
+            this.Dice3.Size = new System.Drawing.Size(121, 95);
+            this.Dice3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice3.TabIndex = 60;
+            this.Dice3.TabStop = false;
+            // 
+            // Dice4
+            // 
+            this.Dice4.Image = global::Snake_and_Ladder.Properties.Resources._4;
+            this.Dice4.Location = new System.Drawing.Point(983, 539);
+            this.Dice4.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice4.Name = "Dice4";
+            this.Dice4.Size = new System.Drawing.Size(121, 95);
+            this.Dice4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice4.TabIndex = 59;
+            this.Dice4.TabStop = false;
+            // 
+            // Dice5
+            // 
+            this.Dice5.Image = global::Snake_and_Ladder.Properties.Resources._5;
+            this.Dice5.Location = new System.Drawing.Point(983, 539);
+            this.Dice5.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice5.Name = "Dice5";
+            this.Dice5.Size = new System.Drawing.Size(121, 95);
+            this.Dice5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice5.TabIndex = 58;
+            this.Dice5.TabStop = false;
+            // 
+            // Dice6
+            // 
+            this.Dice6.Image = global::Snake_and_Ladder.Properties.Resources._6;
+            this.Dice6.Location = new System.Drawing.Point(983, 539);
+            this.Dice6.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice6.Name = "Dice6";
+            this.Dice6.Size = new System.Drawing.Size(121, 95);
+            this.Dice6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice6.TabIndex = 57;
+            this.Dice6.TabStop = false;
+            // 
+            // Dice1
+            // 
+            this.Dice1.Image = global::Snake_and_Ladder.Properties.Resources._1;
+            this.Dice1.Location = new System.Drawing.Point(983, 539);
+            this.Dice1.Margin = new System.Windows.Forms.Padding(4);
+            this.Dice1.Name = "Dice1";
+            this.Dice1.Size = new System.Drawing.Size(121, 95);
+            this.Dice1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Dice1.TabIndex = 56;
+            this.Dice1.TabStop = false;
+            // 
+            // Board1
+            // 
+            this.Board1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Board1.Image = global::Snake_and_Ladder.Properties.Resources.board1;
+            this.Board1.Location = new System.Drawing.Point(1, 0);
+            this.Board1.Margin = new System.Windows.Forms.Padding(4);
+            this.Board1.Name = "Board1";
+            this.Board1.Size = new System.Drawing.Size(1213, 753);
+            this.Board1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Board1.TabIndex = 0;
+            this.Board1.TabStop = false;
+            // 
+            // Board2
+            // 
+            this.Board2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Board2.Image = global::Snake_and_Ladder.Properties.Resources.board2;
+            this.Board2.Location = new System.Drawing.Point(1, 0);
+            this.Board2.Margin = new System.Windows.Forms.Padding(4);
+            this.Board2.Name = "Board2";
+            this.Board2.Size = new System.Drawing.Size(1213, 753);
+            this.Board2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Board2.TabIndex = 0;
+            this.Board2.TabStop = false;
+            // 
+            // Board3
+            // 
+            this.Board3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Board3.Image = global::Snake_and_Ladder.Properties.Resources.board3;
+            this.Board3.Location = new System.Drawing.Point(1, 0);
+            this.Board3.Margin = new System.Windows.Forms.Padding(4);
+            this.Board3.Name = "Board3";
+            this.Board3.Size = new System.Drawing.Size(1213, 753);
+            this.Board3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Board3.TabIndex = 0;
+            this.Board3.TabStop = false;
+            // 
+            // Board4
+            // 
+            this.Board4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Board4.Image = global::Snake_and_Ladder.Properties.Resources.board4;
+            this.Board4.Location = new System.Drawing.Point(1, 0);
+            this.Board4.Margin = new System.Windows.Forms.Padding(4);
+            this.Board4.Name = "Board4";
+            this.Board4.Size = new System.Drawing.Size(1213, 753);
+            this.Board4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Board4.TabIndex = 0;
+            this.Board4.TabStop = false;
+            // 
+            // Board5
+            // 
+            this.Board5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Board5.Image = global::Snake_and_Ladder.Properties.Resources.board5;
+            this.Board5.Location = new System.Drawing.Point(1, 0);
+            this.Board5.Margin = new System.Windows.Forms.Padding(4);
+            this.Board5.Name = "Board5";
+            this.Board5.Size = new System.Drawing.Size(1213, 753);
+            this.Board5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Board5.TabIndex = 0;
+            this.Board5.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 757);
+            this.Controls.Add(this.CurrentPlayer);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Dice1);
+            this.Controls.Add(this.Dice2);
+            this.Controls.Add(this.Dice3);
+            this.Controls.Add(this.Dice4);
+            this.Controls.Add(this.Dice5);
+            this.Controls.Add(this.Dice6);
+            this.Controls.Add(this.Player2Name);
+            this.Controls.Add(this.Player1Name);
+            this.Controls.Add(this.Board1);
+            this.Controls.Add(this.Board2);
+            this.Controls.Add(this.Board3);
+            this.Controls.Add(this.Board4);
+            this.Controls.Add(this.Board5);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "Snake And Ladder (Play Game)";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dice1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Board5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label Player1Name;
+        public System.Windows.Forms.Label Player2Name;
+        public System.Windows.Forms.PictureBox Board1;
+        public System.Windows.Forms.PictureBox Board2;
+        public System.Windows.Forms.PictureBox Board3;
+        public System.Windows.Forms.PictureBox Board4;
+        public System.Windows.Forms.PictureBox Board5;
+        public System.Windows.Forms.PictureBox Dice1;
+        public System.Windows.Forms.PictureBox Dice6;
+        public System.Windows.Forms.PictureBox Dice5;
+        public System.Windows.Forms.PictureBox Dice4;
+        public System.Windows.Forms.PictureBox Dice3;
+        public System.Windows.Forms.PictureBox Dice2;
+        public System.Windows.Forms.Button Reset;
+        public System.Windows.Forms.PictureBox Player1;
+        public System.Windows.Forms.PictureBox Player2;
+        public System.Windows.Forms.Label CurrentPlayer;
+        public System.Windows.Forms.Button Exit;
+        public System.Windows.Forms.Button button2;
+    }
+}
